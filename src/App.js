c5bc1c68e08654e20d56f8203cc98a8eed6e22fb
RamaSai2519 <rchandik@gitam.in>
2024-11-22 18:00:35 +0530

    Add Header component, update Home layout, and enhance Tailwind CSS configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Home from "./Home";
+import Header from "./components/Header";
 import { ConfigProvider, theme } from "antd";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -9,7 +10,8 @@ const App = () => {
 
   return (
     <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
-      <div className="text-white min-w-full min-h-screen overflow-clip bg-black">
+      <div className="text-white min-w-full min-h-screen overflow-clip">
+        <Header />
         <Routes>
           {isAuth ? (
             <>
