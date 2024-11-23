faded61c161178e782338b79f18452ad04bd6183
lechakrawarthy <chakravarthi1597@gmail.com>
2024-11-24 00:52:28 +0530

    Add Info component to Home; display countdown and event details for Epoch 3.0

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Intro from "../screens/Intro";
 import Hero from "../screens/Hero";
 import Faq from "../screens/faq";
+import Info from "../screens/Info";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Info />
             <Intro />
             <Faq />
         </>
