be229fadbf1d3a7f5f3847ba0e1646c030c5c297
lechakrawarthy <chakravarthi1597@gmail.com>
2024-12-12 16:09:28 +0530

    Update event date in Hero component from 2024 to 2025

--- src/screens/Hero/index.js
+++ src/screens/Hero/index.js
@@ -62,7 +62,7 @@ const Hero = () => {
             <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl relative mt-4">
                 <span className="absolute inset-0 blur-[10px] text-blue-400 opacity-70">18th - 19th December | 2024</span>
                 <span className="absolute inset-0 blur-[5px] text-blue-300 opacity-80">18th - 19th December | 2024</span>
-                <span className="relative text-white">18th - 19th December | 2024</span>
+                <span className="relative text-white">18th - 19th December | 2025</span>
             </p>
         </div>
     );
